3a03f6f37da2c51977789ffc6fe84e5745fa81ad
ChirayuRai <chirayurai@gmail.com>
2023-03-28 15:04:49 -0700

    testing cyclic with mongoDB

--- API/index.js
+++ API/index.js
@@ -1,31 +1,38 @@
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000
-const cors = require('cors');
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({ path: ".env" });
 
+app.use(express.json())
 app.use(cors({
-  origin: '*'
+  origin: "*",
 }));
 
-const uri = process.env.MONGO_CONNECTION_STRING;
+const PORT = process.env.PORT || 3000
+const uri = process.env.MONGO_CONN;
 const client = new MongoClient(uri);
 
-app.post("/query", async (req, res) => {
-  let my_query = req.body
-  let query = await client.db("Users")
+
+app.get("/", (req, res) => {
+  return res.json({ "Hello": "World" })
+})
+
+app.post("/query", (req, res) => {
+  let query = client.db("Users")
     .collection("Data")
-    .find(my_query)
+    .find(req.body)
 
   return res.json(query)
 })
 
+
 client.connect(err => {
   if (err) { console.error(err); return false; }
   // connection to mongo is successful, listen for requests
+  console.log("Connected to MongoDB");
   app.listen(PORT, () => {
-    console.log("listening for requests");
+    console.log(`listening for requests on port ${PORT}`);
   })
 });
 
