a5b5682730a55c1600e6a588b8a150f91a333a2c
ChirayuRai <chirayurai@gmail.com>
2023-04-06 14:32:36 -0700

    updated conn string

--- API/db/user.js
+++ API/db/user.js
@@ -36,6 +36,6 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const UserModel = mongoose.model("Usertest", UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
 module.exports = UserModel;
