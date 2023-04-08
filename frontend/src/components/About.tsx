203cb642ef007107a8ac637722959e748974ed45
ChirayuRai <chirayurai@gmail.com>
2023-04-07 23:42:38 -0700

    added about component

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -30,6 +30,29 @@ const About = () => {
               "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 4px 6px rgba(0, 0, 0, 0.25)",
           }}
         >
+          Hæli:
+        </p>
+        <p
+          className="text-left text-white font-medium text-m"
+          style={{
+            fontFamily: "Roboto, sans-serif",
+            letterSpacing: "0.05em",
+            textShadow:
+              "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 4px 6px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          In Icelandic, "hæli" is a noun that translates to "shelter" or "protection" in English. It refers to a place or situation that provides safety from adverse weather, danger, or any other threatening conditions
+        </p>
+        <br />
+        <p
+          className="text-left text-white font-bold text-2xl"
+          style={{
+            fontFamily: "Roboto, sans-serif",
+            letterSpacing: "0.05em",
+            textShadow:
+              "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 4px 6px rgba(0, 0, 0, 0.25)",
+          }}
+        >
           Credits:
         </p>
         <p
@@ -40,7 +63,7 @@ const About = () => {
             textShadow:
               "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 4px 6px rgba(0, 0, 0, 0.25)",
           }}
-        >Amogh Prakash, Chirayu Rai, Gerardo Solis, Andy Diep, Sara Cozart</p>
+        ><a href="https://www.linkedin.com/in/amoghprak/" target="_blank" className="hover:text-slate-300"> Amogh Prakash </a>, <a href="https://www.linkedin.com/in/chirayu-rai/" target="_blank" className="hover:text-slate-300"> Chirayu Rai </a>, <a href="https://www.linkedin.com/in/gerardosolisit/" target="_blank" className="hover:text-slate-300"> Gerardo Solis </a>, <a href="https://www.linkedin.com/in/andy-diep/" target="_blank" className="hover:text-slate-300"> Andy Diep </a>, <a href="https://www.linkedin.com/in/sararosec/" target="_blank" className="hover:text-slate-300"> Sara Cozart </a></p>
         <br />
         <p
           className="text-left text-white font-bold text-2xl"
