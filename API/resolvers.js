5dc35bccffaadf7d3f7122bc088bd95c8df0d2d2
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-04-10 17:55:04 -0700

    Update resolvers.js

--- API/resolvers.js
+++ API/resolvers.js
@@ -2,7 +2,7 @@ const tf = require("@tensorflow/tfjs");
 const { v4: uuidv4 } = require("uuid");
 const Redis = require("ioredis");
 const axios = require("axios");
- const apiKey = process.env.OPEN_API_KEY;
+const apiKey = process.env.OPEN_API_KEY;
 
 
 function getNewRedisClient() {
