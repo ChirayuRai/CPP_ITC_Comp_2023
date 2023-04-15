8997894bf63253b738eb90d5b0880021fe11e15d
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-04-14 22:29:18 -0700

    Update SearchResults.tsx

--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -213,13 +213,14 @@ const SearchResults: React.FC<SearchResultsProps> = ({
                     }}
                     // ref={scrollContainerRef}
                   >
-                    <button
+                    
+                    <div className="text-center mb-4">
+                      <button
                       className="absolute top-4 left-4 text-white focus:outline-none ml-4 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                       onClick={closeDetailedView}
                     >
                       <FontAwesomeIcon icon={faTimes} size="lg" />
                     </button>
-                    <div className="text-center mb-4">
                       <div className="rounded-full mb-4 h-24 w-24 mx-auto">
                         <img
                           src={selectedUser.imgUrl}
